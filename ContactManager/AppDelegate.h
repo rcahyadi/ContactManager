513622dedeb730c4e90dbec82258a187f907f381
rakhmatc <rakhmat.cahyadi@gmail.com>
2017-12-11 05:46:31 +0700

    Initial Commit

---
+++ ContactManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContactManager
+//
+//  Created by rakhmatc on 11/12/17.
+//  Copyright © 2017 rakhmatc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContactManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ContactManager
+//
+//  Created by rakhmatc on 11/12/17.
+//  Copyright © 2017 rakhmatc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
